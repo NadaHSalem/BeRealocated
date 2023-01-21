11cc302420aa8971e122daf06beda6620a2a8350
lucasreljic <lucas.reljic@gmail.com>
2023-01-21 17:49:44 -0500

    changed firebase

--- berealocated/src/pages/Profile.js
+++ berealocated/src/pages/Profile.js
@@ -1,13 +1,11 @@
 import React from "react";
 import styles from "./Profile.module.css";
-import { signInWithGoogle } from "../firebase_setup/firebase";
 
 const Explore = () => {
 
     return (
         <div className={styles.profile}>
             
-            <button className="button" onClick={signInWithGoogle}><i className="fab fa-google"></i>Sign in with google</button>
             </div>
     )
 };
