44f44516be5982538172c05acb133d038cb3c409
lucasreljic <lucas.reljic@gmail.com>
2023-01-21 08:30:31 -0500

    Working capture and retake

--- berealocated/src/pages/Home.js
+++ berealocated/src/pages/Home.js
@@ -1,60 +1,46 @@
-import React from "react";
-import { useState } from 'react'
+import React, { useCallback, useRef, useState } from "react";
 import Webcam from "react-webcam";
 import styles from "./Home.module.css"
-const WebcamComponent = () => <Webcam />
-const videoConstraints = {
-  width: 400,
-  height: 400,
-  facingMode: 'user',
-}
-const Home = () => {
-  const [picture, setPicture] = useState('')
-  const webcamRef = React.useRef(null)
-  const capture = React.useCallback(() => {
-    const pictureSrc = webcamRef.current.getScreenshot()
-    setPicture(pictureSrc)
-  })
+function Home() {
+  const [img, setImg] = useState(null);
+  const webcamRef = useRef(null);
+
+  const videoConstraints = {
+    width: 420,
+    height: 420,
+    facingMode: "user",
+  };
+
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImg(imageSrc);
+  }, [webcamRef]);
+
   return (
-    <div>
-      <div>
-        {picture === '' ? (
+    <div className={styles.container}>
+      {img === null ? (
+        <>
           <Webcam
+            className={styles.camera}
             audio={false}
+            mirrored={true}
             height={500}
-            ref={webcamRef}
             width={500}
+            ref={webcamRef}
             screenshotFormat="image/jpeg"
             videoConstraints={videoConstraints}
           />
-        ) : (
-          <img src={picture} />
-        )}
-      </div>
-      <div>
-        {picture != '' ? (
-          <button
-            onClick={(e) => {
-              e.preventDefault()
-              setPicture()
-            }}
-            className="btn btn-primary"
-          >
-            Retake
-          </button>
-        ) : (
-          <button
-            onClick={(e) => {
-              e.preventDefault()
-              capture()
-            }}
-            className="btn btn-danger"
-          >
-            Capture
-          </button>
-        )}
-      </div>
+          <div className={styles.outercircle}>
+          <button className={styles.capture} onClick={capture}></button></div>
+        </>
+      ) : (
+        <>
+          <img src={img} alt="screenshot" />
+          <button className={styles.retake} onClick={() => setImg(null)}>&#10006;</button>
+        </>
+      )}
     </div>
-  )
+  );
 }
+
 export default Home;
