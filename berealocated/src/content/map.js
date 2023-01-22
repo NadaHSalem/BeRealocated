6c46bfd0c146486c7950ee024294d8c5ed31159d
NadaHSalem <121772796+NadaHSalem@users.noreply.github.com>
2023-01-21 23:55:28 -0500

    working multiple markers

--- berealocated/src/content/map.js
+++ berealocated/src/content/map.js
@@ -1,18 +1,22 @@
 import React from "react";
-import GoogleMapReact from 'google-map-react';
-import google from 'google-map-react';
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import styles from "./map.module.css";
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import google from 'google-map-react';
+import { GoogleMap, useJsApiLoader, Marker, mar } from '@react-google-maps/api';
 
   const containerStyle = {
     width: '500px',
     height: '500px'
   };
-  const center = {
-    lat: 43.634657,
-      lng: -79.522378
+  const defaultPoint = {
+    lat:43.634657,
+    lng: -79.522378
   };
+  const points = [
+    {name: "1", position: {lat: 43.634657, lng:-79.522378}},
+    {position: {lat: 43.642567, lng:-79.387054}},
+      {position: {lat: 43.725600, lng:-79.4527}}
+  ]
   function SimpleMap(props) {
     const { isLoaded } = useJsApiLoader({
       id: 'google-map-script',
@@ -20,25 +24,50 @@ import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
     })
     const [map, setMap] = React.useState(null)
     const onLoad = React.useCallback(function callback(map) {
-      const bounds = new window.google.maps.LatLngBounds(center);
-      map.fitBounds(bounds);
+      map.setZoom(10)
       setMap(map)
     }, [])
     const onUnmount = React.useCallback(function callback(map) {
       setMap(null)
     }, [])
+    var markers = [];
+    let i = 0;
+    var marker = useState(null);
+    const Iterate = () =>
+    {
 
+      const latLngArray = [];
+      for (i = 0; i < points.length-1; i++){
+        const location = new google.maps(points[i][0], points[i][1]);
+        latLngArray.push(location);
+        marker = new google.maps.Marker({
+          position: location,
+          map: map
+      });
+      markers.push(marker);
+      }
+
+    }
     return isLoaded ? (
         <GoogleMap
           mapContainerStyle={containerStyle}
-          center={center}
-          zoom={1}
+          center={defaultPoint}
+          defaultZoom={10}
           onLoad={onLoad}
           onUnmount={onUnmount}
         >
-          { props.isMarkerShown && <Link to="explore"><Marker position={{ lat: 43.634657, lng: -79.522378 }} /></Link>/* Child components, such as markers, info windows, etc. */ }
-          <></>
+          
+          {points.map(({ id, name, position }) => (
+        <Marker
+          key={id}
+          position={position}
+          label={name}
+        >
+        </Marker>
+      ))}
+        
         </GoogleMap>
+        
     ) : <></>
   }
 export default SimpleMap;
